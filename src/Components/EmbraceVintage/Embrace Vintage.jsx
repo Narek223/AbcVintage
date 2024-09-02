b4aad0994dd3e7857cfa2ea06998e78f06e10dbb
Narek223 <panosyan-narek@mail.ru>
2024-09-02 14:39:29 +0400

    gallary page

---
+++ src/Components/EmbraceVintage/Embrace Vintage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './embracevintage.module.scss'
+
+export default function EmbraceVintage() {
+  return (
+    <div className={styles.EmbraceVintageComponent}>
+<div className={styles.vintageElegance}>
+    <h1>Embrace Vintage<br/> Elegance</h1>
+    <button>Shop now</button>
+</div>
+
+
+    </div>
+  )
+}
--- src/Routes/Routefiles.jsx
+++ src/Routes/Routefiles.jsx
@@ -3,7 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../Components/Home/Home";
 import AboutUs from "../Components/AboutUs/AboutUs";
 import  Benefits from '../Components/Benefits/Benefits'
-
+import EmbraceVintage from "../Components/EmbraceVintage/Embrace Vintage";
+import Gallery from "../Components/Gallery/Gallery";
 export default function Routefiles() {
   return (
     <div>
@@ -15,6 +16,8 @@ export default function Routefiles() {
       <Home/>
       <AboutUs/>
       <Benefits/>
+      <EmbraceVintage/>
+      <Gallery/>
     </div>
   );
 }
