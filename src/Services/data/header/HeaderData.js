3215f31f2f34b4c5e285025a9979ba3690212fdc
Narek223 <panosyan-narek@mail.ru>
2024-09-09 20:44:09 +0400

    footer

--- src/Services/data/header/HeaderData.js
+++ src/Services/data/header/HeaderData.js
@@ -4,8 +4,8 @@ import facebookLogo from '../../../assets/Header/facebook.png'
 
 
 export const socialMediaIcons = [
-    { id:1,src: instagramlogo, alt: 'Instagram Logo' },
-    { id:2,src: pinterestLogo, alt: 'Pinterest Logo' },
-    { id:3,src: facebookLogo, alt: 'Facebook Logo' }
+    { id:1,src: instagramlogo, alt: 'Instagram Logo', text:'Instagram' },
+    { id:2,src: pinterestLogo, alt: 'Pinterest Logo',text:'Pinterest' },
+    { id:3,src: facebookLogo, alt: 'Facebook Logo',text:"Facebook" }
     
   ];
